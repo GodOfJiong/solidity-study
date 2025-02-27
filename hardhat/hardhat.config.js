a2e467aa1e2e1f54d17d40153b7937c38b96dbbc
habon <1033932438@qq.com>
2025-02-27 19:54:19 +0800

    npx hardhat run scripts/deployCrowdFund.js --network sepolia

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/oc_isVT1GRDemrNJqLQBawc2TbHLuZ2n",
+      accounts: ["a074405f4c8a648d25ffe73d10a6ce0f09d05be8c37bc6d4d5c0c581a32ba386"]
+    }
+  }
 };
