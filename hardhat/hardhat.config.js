39df4b06775f697ed432d0a391ecfd7e22264e7a
habon <1033932438@qq.com>
2025-02-27 20:23:19 +0800

    .env

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/oc_isVT1GRDemrNJqLQBawc2TbHLuZ2n",
-      accounts: ["a074405f4c8a648d25ffe73d10a6ce0f09d05be8c37bc6d4d5c0c581a32ba386"]
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.TEST_ACCOUNT_1]
     }
   }
 };
