d70917967718ff4afbcb10690226ec9d06f0b959
habon <1033932438@qq.com>
2025-05-17 13:36:42 +0800

    more better const use

--- lesson6/deploy/2-deployCrossChainTokenPool.js
+++ lesson6/deploy/2-deployCrossChainTokenPool.js
@@ -4,24 +4,29 @@ const {devNetList, netCfgMap} = require("../complexConfig");
 module.exports = async ({network, getNamedAccounts, deployments}) => {
     const {testAccount1} = await getNamedAccounts();
     const {deploy, log} = deployments;
-    let ccipAddr;
+    let srcRouterAddr;
+    let linkAddr;
     if (devNetList.includes(network.name)) {
-        ccipAddr = (await deployments.get("CCIPLocalSimulator")).address;
+        const ccipAddr = (await deployments.get(process.env.CONTRACT_NAME_MOCK_CCIP)).address;
+        const ccip = await ethers.getContractAt(process.env.CONTRACT_NAME_MOCK_CCIP, ccipAddr);
+        const ccipCfg = await ccip.configuration();
+        srcRouterAddr = ccipCfg.sourceRouter_;
+        linkAddr = ccipCfg.linkToken_;
     } else {
-        ccipAddr = netCfgMap[network.config.chainId].ccip;
+        srcRouterAddr = netCfgMap[network.config.chainId].router;
+        linkAddr = netCfgMap[network.config.chainId].link;
     }
 
-    const ccip = await ethers.getContractAt("CCIPLocalSimulator", ccipAddr);
-    const ccipCfg = await ccip.configuration();
-    const cctAddr = (await deployments.get("CrossChainToken")).address;
+    
+    const cctAddr = (await deployments.get(process.env.CONTRACT_NAME_CCT)).address;
     log("deploy CrossChainTokenPool start");
-    const crossChainTokenPool = await deploy("CrossChainTokenPool", {
+    const crossChainTokenPool = await deploy(process.env.CONTRACT_NAME_CCTP, {
         from: testAccount1,
-        args: [ccipCfg.sourceRouter_, ccipCfg.linkToken_, cctAddr],
+        args: [srcRouterAddr, linkAddr, cctAddr],
         log: true
     });
 
     log("deploy CrossChainTokenPool done");
 }
 
-module.exports.tags = ["all", "sourcechain"];
+module.exports.tags = [process.env.DEPLOY_TAG_ALL, process.env.DEPLOY_TAG_SRC_CHAIN];
--- lesson6/deploy/4-deployWrappedCrossChainTokenPool.js
+++ lesson6/deploy/4-deployWrappedCrossChainTokenPool.js
@@ -4,24 +4,28 @@ const {devNetList, netCfgMap} = require("../complexConfig");
 module.exports = async ({network, getNamedAccounts, deployments}) => {
     const {testAccount1} = await getNamedAccounts();
     const {deploy, log} = deployments;
-    let ccipAddr;
+    let destRouterAddr;
+    let linkAddr;
     if (devNetList.includes(network.name)) {
-        ccipAddr = (await deployments.get("CCIPLocalSimulator")).address;
+        const ccipAddr = (await deployments.get(process.env.CONTRACT_NAME_MOCK_CCIP)).address;
+        const ccip = await ethers.getContractAt(process.env.CONTRACT_NAME_MOCK_CCIP, ccipAddr);
+        const ccipCfg = await ccip.configuration();
+        destRouterAddr = ccipCfg.destinationRouter_;
+        linkAddr = ccipCfg.linkToken_;
     } else {
-        ccipAddr = netCfgMap[network.config.chainId].ccip;
+        destRouterAddr = netCfgMap[network.config.chainId].router;
+        linkAddr = netCfgMap[network.config.chainId].link;
     }
 
-    const ccip = await ethers.getContractAt("CCIPLocalSimulator", ccipAddr);
-    const ccipCfg = await ccip.configuration();
-    const wcctAddr = (await deployments.get("WrappedCrossChainToken")).address;
+    const wcctAddr = (await deployments.get(process.env.CONTRACT_NAME_WCCT)).address;
     log("deploy WrappedCrossChainTokenPool start");
-    const wrappedCrossChainTokenPool = await deploy("WrappedCrossChainTokenPool", {
+    const wrappedCrossChainTokenPool = await deploy(process.env.CONTRACT_NAME_WCCTP, {
         from: testAccount1,
-        args: [ccipCfg.destinationRouter_, ccipCfg.linkToken_, wcctAddr],
+        args: [destRouterAddr, linkAddr, wcctAddr],
         log: true
     });
 
     log("deploy WrappedCrossChainTokenPool done");
 }
 
-module.exports.tags = ["all", "destinationchain"];
+module.exports.tags = [process.env.DEPLOY_TAG_ALL, process.env.DEPLOY_TAG_DEST_CHAIN];
