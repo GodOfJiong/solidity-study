fafe390a5a4b27f58d730e9f6c4d427310d334eb
habon <1033932438@qq.com>
2025-05-18 17:32:35 +0800

    deploy on real multi test net

--- lesson6/complexConfig.js
+++ lesson6/complexConfig.js
@@ -6,6 +6,11 @@ netCfgMap[Number(process.env.SEPOLIA_ID)] = {
     router: "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59",
     link: "0x779877A7B0D9E8603169DdbD7836e478b4624789"
 }
+netCfgMap[Number(process.env.AMOY_ID)] = {
+    name: "amoy",
+    router: "0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2",
+    link: "0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904"
+}
 
 module.exports = {
     devNetList,
--- lesson6/hardhat.config.js
+++ lesson6/hardhat.config.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("@chainlink/env-enc").config();
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
 require("hardhat-deploy");
@@ -7,6 +8,18 @@ require("hardhat-deploy-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      chainId: Number(process.env.SEPOLIA_ID),
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.TEST_ACCOUNT_1]
+    },
+    amoy: {
+      chainId: Number(process.env.AMOY_ID),
+      url: process.env.AMOY_URL,
+      accounts: [process.env.TEST_ACCOUNT_1]
+    }
+  },
   namedAccounts: {
     testAccount1: {
       default: 0
